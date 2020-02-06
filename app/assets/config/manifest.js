ab0067f4e0e5c173dbb1ffbca9c1c5fdcee6a6e2
Andrei <1234and95@gmail.com>
2020-02-06 21:49:18 +0300

    MainRAils

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,9 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= require jquery
+//= require bootstrap-sprockets
+ //= link application.js
+
+ //= require jquery3
+//= require popper
+//= require bootstrap
---
+++ app/assets/javascript/application.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require bootstrap-sprockets
+
+//= require jquery3
+//= require popper
+//= require bootstrap
